3de19a382ad84d703941bc03549fec1fffb79573
caseysparkz <himself@caseyspar.kz>
2025-02-28 14:54:29 -0800

    style(centripedalporch.com/frontend.tf): renamed resources for easier migration later

--- centripedalporch.com/frontend.tf
+++ centripedalporch.com/frontend.tf
@@ -8,8 +8,8 @@ data "aws_iam_policy_document" "s3_public_read_access" {
     sid     = "PublicReadGetObject"
     actions = ["s3:GetObject"]
     resources = [
-      aws_s3_bucket.www_site.arn,
-      "${aws_s3_bucket.www_site.arn}/*",
+      aws_s3_bucket.carrd_site.arn,
+      "${aws_s3_bucket.carrd_site.arn}/*",
     ]
     principals {
       type        = "AWS"
@@ -33,7 +33,7 @@ resource "cloudflare_dns_record" "cname_web_root" {
 resource "cloudflare_dns_record" "cname_www_root" {
   zone_id = local.cloudflare_zone_id
   name    = "www.${var.root_domain}"
-  content = aws_s3_bucket_website_configuration.www_site.website_endpoint
+  content = aws_s3_bucket_website_configuration.carrd_site.website_endpoint
   type    = "CNAME"
   ttl     = 1
   proxied = false
@@ -42,44 +42,44 @@ resource "cloudflare_dns_record" "cname_www_root" {
 
 # S3 --------------------------------------------------------------------------
 # WWW site --------------------------------------------------------------------
-resource "aws_s3_bucket" "www_site" {
+resource "aws_s3_bucket" "carrd_site" {
   bucket        = "www.${var.root_domain}"
   force_destroy = true
   tags          = local.common_tags
 }
 
-resource "aws_s3_bucket_website_configuration" "www_site" {
-  bucket = aws_s3_bucket.www_site.id
+resource "aws_s3_bucket_website_configuration" "carrd_site" {
+  bucket = aws_s3_bucket.carrd_site.id
 
   index_document { suffix = "index.html" }
   error_document { key = "404.html" }
 }
 
-resource "aws_s3_bucket_ownership_controls" "www_site" {
-  bucket = aws_s3_bucket.www_site.id
+resource "aws_s3_bucket_ownership_controls" "carrd_site" {
+  bucket = aws_s3_bucket.carrd_site.id
 
   rule { object_ownership = "BucketOwnerPreferred" }
 }
 
-resource "aws_s3_bucket_public_access_block" "www_site" {
-  bucket                  = aws_s3_bucket.www_site.id
+resource "aws_s3_bucket_public_access_block" "carrd_site" {
+  bucket                  = aws_s3_bucket.carrd_site.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_acl" "www_site" {
+resource "aws_s3_bucket_acl" "carrd_site" {
   depends_on = [
-    aws_s3_bucket_ownership_controls.www_site,
-    aws_s3_bucket_public_access_block.www_site,
+    aws_s3_bucket_ownership_controls.carrd_site,
+    aws_s3_bucket_public_access_block.carrd_site,
   ]
-  bucket = aws_s3_bucket.www_site.id
+  bucket = aws_s3_bucket.carrd_site.id
   acl    = "public-read"
 }
 
-resource "aws_s3_bucket_policy" "www_site" {
-  bucket = aws_s3_bucket.www_site.id
+resource "aws_s3_bucket_policy" "carrd_site" {
+  bucket = aws_s3_bucket.carrd_site.id
   policy = data.aws_iam_policy_document.s3_public_read_access.json
 }
 
@@ -93,18 +93,18 @@ resource "aws_s3_bucket" "web_root" {
 resource "aws_s3_bucket_website_configuration" "web_root" {
   bucket = aws_s3_bucket.web_root.id
 
-  redirect_all_requests_to { host_name = aws_s3_bucket.www_site.id }
+  redirect_all_requests_to { host_name = aws_s3_bucket.carrd_site.id }
 }
 
 # Outputs =====================================================================
 output "aws_s3_bucket_endpoint" {
   description = "Bucket endpoint"
-  value       = aws_s3_bucket_website_configuration.www_site.website_endpoint
+  value       = aws_s3_bucket_website_configuration.carrd_site.website_endpoint
   sensitive   = false
 }
 
 output "aws_s3_bucket_id" {
   description = "ID of the S3 bucket (as expected by the AWS CLI)."
-  value       = aws_s3_bucket.www_site.id
+  value       = aws_s3_bucket.carrd_site.id
   sensitive   = false
 }
